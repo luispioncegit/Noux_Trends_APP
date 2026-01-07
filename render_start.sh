a01956fa1992711430cd4b2ddd73606aebe1ee71
Luis Enrique Pionce <90711992+luispioncegit@users.noreply.github.com>
2026-01-07 16:59:49 -0500

    Delete render_start.sh

--- render_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 1. Iniciar el Backend en segundo plano (usando &)
-# Ajusta 'backend/main.py' si tu archivo principal tiene otro nombre
-python backend/main.py &
-
-# 2. Iniciar el Frontend de Streamlit
-# Usamos $PORT porque Render asigna un puerto dinámico
-streamlit run frontend/main.py --server.port $PORT --server.address 0.0.0.0
